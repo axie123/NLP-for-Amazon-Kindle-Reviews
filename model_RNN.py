2facdb15168ac4bc866f1970b9850f68b6e7aafd
gottagitgoing <ruo.cxie@gmail.com>
2019-11-15 11:44:05 -0500

    Added another version of the RNN.

---
+++ model_RNN.py
@@ -0,0 +1,22 @@
+# This is the RNN used for our project:
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class RNN(nn.Module):
+    def __init__(self, embedding_dim, vocab, hidden_dim):
+        super(RNN, self).__init__()
+        self.embedding = nn.Embedding.from_pretrained(vocab.vectors)
+        self.h_dim = hidden_dim
+        self.GRU = nn.GRU(embedding_dim, hidden_dim)
+        self.fc1 = nn.Linear(hidden_dim, 5)
+        self.fc2 = nn.Linear(5,1)
+
+
+    def forward(self, x, lengths=None):
+        embedded = self.embedding(x)
+        embedded = nn.utils.rnn.pack_padded_sequence(embedded, lengths=lengths)
+        _, x = self.GRU(embedded)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
